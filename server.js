de10441ef464208a1ce36533abf2239f99d34170
JaydeepW <jaydeep.w@gmail.com>
2022-03-21 13:13:40 +0530

    some variable naming

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const Web3 = require('web3')
-const { addressProviderABI } = require('./abis')
+const { lendingPoolProviderAbi, lendingPoolAbi } = require('./abis')
 const { KOVAN_LENDING_POOL_PROVIDER_ADDRESS } = require('./addresses')
 const rpcURL = "https://kovan.infura.io/v3/"+process.env.INFURA_KEY
 const web3 = new Web3(rpcURL)
@@ -15,9 +15,7 @@ app.listen(3000, () => console.log('Server Started'))
 app.get("/", (req, res) => {
     web3.eth.getBalance(address, async (err, wei) => {
         balance = web3.utils.fromWei(wei, 'ether')
-        console.log(addressProviderABI)
-        // res.send(addressProviderABI)
-        const providerInstance = new web3.eth.Contract(addressProviderABI, KOVAN_LENDING_POOL_PROVIDER_ADDRESS)
+        const providerInstance = new web3.eth.Contract(lendingPoolProviderAbi, KOVAN_LENDING_POOL_PROVIDER_ADDRESS)
         const lendingPoolAddress = await providerInstance.methods.getLendingPool().call()
         .catch((e) => {
             throw Error(`Error getting lendingPool address: ${e.message}`)
